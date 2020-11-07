fef1392904c4ac10aa6c37aa5ddf3c18a816f4f2
creaciones21ls <67108021+creaciones21ls@users.noreply.github.com>
2020-11-07 14:24:28 -0500

    Delete conexion.php

--- conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$server="127.0.0.1";
-	$username="root";
-	$password="";
-	$db='clasesarduino';
-	$con=mysqli_connect($server,$username,$password,$db)or die("no se ha podido establecer la conexion");
-
-?>
